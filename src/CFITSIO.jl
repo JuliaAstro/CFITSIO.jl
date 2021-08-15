52984b02aeabcccc8b720571187605a8baba23e7
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2021-08-16 02:22:57 +0400

    Update src/CFITSIO.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- src/CFITSIO.jl
+++ src/CFITSIO.jl
@@ -245,7 +245,7 @@ Create and open a new empty output `FITSFile`. This methods uses the
 [extended file name syntax](https://heasarc.gsfc.nasa.gov/docs/software/fitsio/c/c_user/node83.html)
 to create the file.
 
-See also [fits_create_diskfile](@ref) which does not use the extended filename parser.
+See also [`fits_create_diskfile`](@ref) which does not use the extended filename parser.
 """
 function fits_create_file(filename::AbstractString)
     ptr = Ref{Ptr{Cvoid}}()
